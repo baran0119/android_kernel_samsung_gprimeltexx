6a9b259aabe31e4a90a7278e3c20b9afbbc4ab0f
baran0119 <baran0119@gmail.com>
2016-10-02 19:57:33 +0200

    Update all

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-mkdir output
+#export CROSS_COMPILE=...
+outDir="../output"
 
-make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+# always clean build
+if [ -d ${outDir} ]; then
+	rm -fr ${outDir}
+fi
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+mkdir ${outDir}
+
+make -C $(pwd) O=${outDir} msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+time make -j4 -C $(pwd) O=${outDir}
